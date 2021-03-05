a3c9d596d43e55a3ea2189f44f1aa9ad2249d9c4
samdani-srabon <samdani.srabon@gmail.com>
2021-03-05 21:45:04 +0600

    time corrected

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("April 12, 2021 15:37:25").getTime();
+var countDownDate = new Date("April 12, 2021 18:20:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
