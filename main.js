94da6131258f6e1cf395ed4ef8d7bce868117d22
samdani-srabon <samdani.srabon@gmail.com>
2021-03-10 23:57:32 +0600

    time updated to bd time 14 April

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("April 12, 2021 18:20:00").getTime();
+var countDownDate = new Date("April 14, 2021 18:20:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
